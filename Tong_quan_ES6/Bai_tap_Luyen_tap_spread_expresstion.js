f54175163bc4517b1479ed56577824026df8d249
langochai <langochai11121998@gmail.com>
2023-04-03 15:57:56 +0700

    update

--- Tong_quan_ES6/Bai_tap_Luyen_tap_spread_expresstion.js
+++ Tong_quan_ES6/Bai_tap_Luyen_tap_spread_expresstion.js
@@ -1,6 +1,5 @@
 function replace(arr,index,secondArr) {
-    arr.splice(index,arr.length-index)
-    arr.push(...secondArr)
+    arr.splice(index,arr.length-index,...secondArr)
 }
 
 let testArray = [1, 2, 100, 100, 6]
@@ -10,22 +9,22 @@ replace(testArray, 2, [3, 4, 5])
 console.log(testArray)
 
 
-function copyReplace(array, index, secondArr) {
-    array.splice(index,array.length)
-    return [...array,...secondArr]
-}
-
-console.log(copyReplace([1, 2, 100, 200, 6], 2,  [3, 4, 5]))
-
-
-
-
-let customerOrder = []
-
-function recordOrders(time,arr) {
-    customerOrder.push(time,...arr)
-}
-
-recordOrders(new Date, ["coffee", "yogurt", "pizza"])
-
-console.log(customerOrder)
+// function copyReplace(array, index, secondArr) {
+//     array.splice(index,array.length)
+//     return [...array,...secondArr]
+// }
+//
+// console.log(copyReplace([1, 2, 100, 200, 6], 2,  [3, 4, 5]))
+//
+//
+//
+//
+// let customerOrder = []
+//
+// function recordOrders(time,arr) {
+//     customerOrder.push(time,...arr)
+// }
+//
+// recordOrders(new Date, ["coffee", "yogurt", "pizza"])
+//
+// console.log(customerOrder)
