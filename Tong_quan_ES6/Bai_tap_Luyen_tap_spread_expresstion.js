8fe7b1ce18312ae60ba772158c0ebe4cc1d5fd7c
langochai <langochai11121998@gmail.com>
2023-04-03 16:57:23 +0700

    update

--- Tong_quan_ES6/Bai_tap_Luyen_tap_spread_expresstion.js
+++ Tong_quan_ES6/Bai_tap_Luyen_tap_spread_expresstion.js
@@ -1,31 +1,41 @@
-function replace(arr,index,secondArr) {
-    arr.splice(index,2,...secondArr)
-}
-
-let testArray = [1, 2, 100, 100, 6]
-
-replace(testArray, 2, [3, 4, 5])
-
-console.log(testArray)
-
-
-function copyReplace(array, from, to, secondArr) {
-    let arr1 = array.slice(0,from)
-    let arr2 = array.slice(to)
-    return [...arr1,...secondArr,...arr2]
-}
-
-console.log(copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]))
+// function replace(arr,index,secondArr) {
+//     arr.splice(index,2,...secondArr)
+// }
+//
+// let testArray = [1, 2, 100, 100, 6]
+//
+// replace(testArray, 2, [3, 4, 5])
+//
+// console.log(testArray)
+//
+//
+// function copyReplace(array, from, to, secondArr) {
+//     let arr1 = array.slice(0,from)
+//     let arr2 = array.slice(to)
+//     return [...arr1,...secondArr,...arr2]
+// }
+//
+// console.log(copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]))
 
 
 
 
 let customerOrder = []
 
-function recordOrders(time,arr) {
-    customerOrder.push(time,...arr)
+function recordOrders(tg,...str) {
+    customerOrder.push({time:tg,birds:str})
 }
-
-recordOrders(new Date, ["coffee", "yogurt", "pizza"])
-
+recordOrders(new Date, "coffee", "yogurt", "pizza")
 console.log(customerOrder)
+
+// let customerOrder = []
+//
+// function recordOrders(time) {
+//
+//     customerOrder.push({time, birds: Array.prototype.slice.call(arguments, 1)})
+//
+// }
+//
+// recordOrders(new Date, "coffee", "yaourt", "pizza")
+//
+// console.log(customerOrder)
