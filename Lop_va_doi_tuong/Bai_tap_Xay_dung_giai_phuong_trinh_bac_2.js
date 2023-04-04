d8594b9dbc543b72e8d2b5ca8a5e94b15e24dccd
langochai <langochai11121998@gmail.com>
2023-04-04 10:17:20 +0700

    update

--- Lop_va_doi_tuong/Bai_tap_Xay_dung_giai_phuong_trinh_bac_2.js
+++ Lop_va_doi_tuong/Bai_tap_Xay_dung_giai_phuong_trinh_bac_2.js
@@ -11,6 +11,25 @@ class QuadraticEquation {
         return (Math.pow(this.b,2) - 4 * this.a * this.c)
     }
     getRoot1(){
-        return ()
+        return ((-this.b + Math.sqrt(Math.pow(this.b,2)-4*this.a*this.c))/(2*this.a))
+    }
+    getRoot2(){
+        return ((-this.b - Math.sqrt(Math.pow(this.b,2)-4*this.a*this.c))/(2*this.a))
+    }
+    getResult(){
+        if(this.getDiscriminant()>0){
+            console.log(this.getRoot1())
+            console.log(this.getRoot2())
+        } else if (this.getDiscriminant()===0){
+            console.log(this.getRoot1())
+        } else {
+            console.log('The equation has no roots')
+        }
     }
 }
+let equal1 = new QuadraticEquation(1,0,-1)
+equal1.getResult()
+let equal2 = new QuadraticEquation(1,-2,1)
+equal2.getResult()
+let equal3 = new QuadraticEquation(1,2,5)
+equal3.getResult()
