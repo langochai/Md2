1f8311e00c580295171305e5b3b5f9f38f969e75
langochai <langochai11121998@gmail.com>
2023-04-04 11:36:52 +0700

    Update Bai_tap_Xay_dung_lop_StopWatch.js

--- Lop_va_doi_tuong/Bai_tap_Xay_dung_lop_StopWatch.js
+++ Lop_va_doi_tuong/Bai_tap_Xay_dung_lop_StopWatch.js
@@ -1,19 +1,47 @@
 class StopWatch{
-    start = new Date()
+    start = new Date().getTime()
     end
     constructor() {
     }
     startW(){
-        this.start = new Date()
+        this.start = new Date().getTime()
     }
     endW(){
-        this.end = new Date()
+        this.end = new Date().getTime()
     }
     getElapsedTime(){
         return (this.end - this.start)
     }
 }
 let watch1 = new StopWatch()
+
+
+function selectionSort(arr) {
+    let currentValueNewIndex;
+    for (let i = 0; i < arr.length; i++) {
+        currentValueNewIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[currentValueNewIndex] > arr[j]) {
+                currentValueNewIndex = j;
+            }
+        }
+        if (i !== currentValueNewIndex) {
+            let temp = arr[i];
+            arr[i] = arr[currentValueNewIndex];
+            arr[currentValueNewIndex] = temp;
+        }
+    }
+
+    return arr;
+}
+const arr = [];
+for (let i=0; i<100000; i++) {
+    arr.push(Math.floor(Math.random() * 100000 + 1));
+}
+
+
+
 watch1.startW()
+console.log(selectionSort(arr));
 watch1.endW()
 console.log(watch1.getElapsedTime())
