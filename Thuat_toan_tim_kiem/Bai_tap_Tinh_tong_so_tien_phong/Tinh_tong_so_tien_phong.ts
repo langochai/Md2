71921d0ef88a1c5686a63dfba3f03ebec5f32c61
langochai <langochai11121998@gmail.com>
2023-04-18 11:41:12 +0700

    update

--- Thuat_toan_tim_kiem/Bai_tap_Tinh_tong_so_tien_phong/Tinh_tong_so_tien_phong.ts
+++ Thuat_toan_tim_kiem/Bai_tap_Tinh_tong_so_tien_phong/Tinh_tong_so_tien_phong.ts
@@ -2,12 +2,17 @@ let matrix: number [][] = [[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]]
 
 function noHauntedRoom(arr: number[][]): number {
     let sum: number = 0
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (i == 0 && arr[i][j] !== 0) {
-                sum += arr[i][j]
-            } else if (arr[i][j] != 0 && arr[i - 1][j] != 0) {
-                sum += arr[i][j]
+    for (let f = 0; f < arr[0].length; f++) {
+        if (arr[0][f] != 0) sum += arr[0][f]
+    }
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] != 0) {
+                let flag: boolean = true
+                for (let k = i - 1; k >= 0; k--) {
+                    if (arr[k][j] == 0) flag = false
+                }
+                if (flag) sum += arr[i][j]
             }
         }
     }
