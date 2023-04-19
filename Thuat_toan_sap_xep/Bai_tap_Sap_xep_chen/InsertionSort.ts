0d2ddef7c4b01398f8a650fdde0d20ee9669faed
langochai <langochai11121998@gmail.com>
2023-04-19 16:54:15 +0700

    update

--- Thuat_toan_sap_xep/Bai_tap_Sap_xep_chen/InsertionSort.ts
+++ Thuat_toan_sap_xep/Bai_tap_Sap_xep_chen/InsertionSort.ts
@@ -5,13 +5,13 @@ export class InsertionSort{
     }
     insertionSort(){
         for (let i = 1; i < this.arr.length; i++) {
-            let current = this.arr[i];
+            let key = this.arr[i];
             let j = i-1;
-            while ((j > -1) && (current < this.arr[j])) {
+            while ((j > -1) && (key < this.arr[j])) {
                 this.arr[j+1] = this.arr[j];
                 j--;
             }
-            this.arr[j+1] = current;
+            this.arr[j+1] = key;
         }
     }
 }
