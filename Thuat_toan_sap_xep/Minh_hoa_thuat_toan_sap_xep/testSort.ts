302529695496a2f6c6dc65d8ff963ff2e12773a2
langochai <langochai11121998@gmail.com>
2023-04-19 13:51:01 +0700

    update

--- Thuat_toan_sap_xep/Minh_hoa_thuat_toan_sap_xep/testSort.ts
+++ Thuat_toan_sap_xep/Minh_hoa_thuat_toan_sap_xep/testSort.ts
@@ -4,11 +4,11 @@ import {InsertionSort} from "./InsertionSort";
 
 let arr = [1,3,5,4,2,6]
 
-// let bubble = new BubbleSort(arr)
-// bubble.bubbleSort()
-//
-// let select = new SelectionSort(arr)
-// select.selectionSort()
+let bubble = new BubbleSort(arr)
+bubble.bubbleSort()
+
+let select = new SelectionSort(arr)
+select.selectionSort()
 
 let insert = new InsertionSort(arr)
 insert.insertionSort()
